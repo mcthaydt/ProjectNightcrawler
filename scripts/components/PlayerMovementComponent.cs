e413c55e5875d1be11e82445443c3b97f37eaed9
Dawson M <107786093+mcthaydt@users.noreply.github.com>
2023-09-27 19:16:42 -0500

    Added PlayerMovementComponent back

---
+++ scripts/components/PlayerMovementComponent.cs
@@ -0,0 +1,40 @@
+using Godot;
+
+public partial class PlayerMovementComponent : Node3D
+{
+	[Export] public Resource PlayerData; 
+	private Vector3 _playerVelocity = Vector3.Zero;
+	
+	public override void _Process(double delta)
+	{
+		if (PlayerData is BaseCharacterData baseCharacterData)
+		{
+			_playerVelocity = baseCharacterData.Velocity;
+		}
+		else
+		{
+			GD.Print("Missing Player Data!");
+			return;
+		}
+		
+		_playerVelocity = Vector3.Zero; 
+		if (Input.IsActionPressed("move_right"))
+		{
+			_playerVelocity += Vector3.Forward;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			_playerVelocity += Vector3.Back;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			_playerVelocity += Vector3.Right;
+		}
+		if (Input.IsActionPressed("move_up"))
+		{
+			_playerVelocity += Vector3.Left;
+		}
+		_playerVelocity = _playerVelocity.Normalized();
+		baseCharacterData.Velocity = _playerVelocity;
+	}
+}
